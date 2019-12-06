b3cd0c2a86c17f2e8e8ffafb4656dcf900ac2689
hitomi numari <h.h.hitomi1216@gmail.com>
2019-12-06 15:29:39 +0900

    仮コード

--- janken.rb
+++ janken.rb
@@ -8,11 +8,15 @@ class Player
   input_hand = gets.chomp
   if input_hand != "0" && input_hand != "1" && input_hand != "2"
     player = Player.new
+    #「グー、チョキ、パー以外の値入力時には、もう一度ジャンケンをする」という条件の時に、playerクラスをインスタンス化し、
     player.hand
+    #handメソッドを呼び出すことで繰り返す。※クラスをnewしないと呼び出せない。
   else
     player_hand = input_hand.to_i
-    puts "a:#{player_hand}"
+    puts "#{player_hand}"
     end
+    return player_hand
+    #returnしないとponメソッドに値渡しできない。
   end
 end
 
@@ -26,10 +30,10 @@ end
 
 class Janken
   def pon(player_hand, enemy_hand)
-    puts "b:#{player_hand}"
-    puts "c:#{enemy_hand}"
+    # puts "b:#{player_hand}"
+    # puts "c:#{enemy_hand}"
     result = (player_hand - enemy_hand +3)%3
-    puts "d:#{result}"
+    # puts "d:#{result}"
     janken = {0=>"グー",1=>"チョキ",2=>"パー"}
     if result == 2
       puts "相手の手は#{janken[enemy_hand]}です。あなたの勝ちです。"
@@ -50,5 +54,5 @@ player = Player.new
 enemy = Enemy.new
 janken = Janken.new
 
-# 下記の記述で、ジャンケンメソッドが起動される
+
 janken.pon(player.hand, enemy.hand)
