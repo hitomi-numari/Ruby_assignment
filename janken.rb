5442af1fb826f13357e9db41c1732b8bfec50ac7
hitomi numari <h.h.hitomi1216@gmail.com>
2019-12-08 17:22:27 +0900

    submit_assignment

--- janken.rb
+++ janken.rb
@@ -1,26 +1,26 @@
-class Player
-  def hand
-    puts "数字を入力してください。"
-    janken = {0=>"グー",1=>"チョキ",2=>"パー"}
-    janken.each do |key, value|
-      puts "#{key}:#{value}"
-    end
-  input_hand = gets.chomp
-  #gets.to_iだと文字列は0と認識する為、あえてgetsで文字列を取得。chompを入れないと改行も文字として認識する為。
-  if input_hand != "0" && input_hand != "1" && input_hand != "2"
-    player = Player.new
-    #「グー、チョキ、パー以外の値入力時には、もう一度ジャンケンをする」という条件の時に、playerクラスをインスタンス化し、
-    player.hand
-    #handメソッドを呼び出すことで繰り返す。※クラスをnewしないと呼び出せない。
-  else
-    player_hand = input_hand.to_i
-    puts "#{player_hand}"
-    end
-    return player_hand
-    #returnしないとponメソッドに値渡しできない。
+# require 'byebug'
+
+class Interface
+  def first
+   puts "数字を入力してください。"
+  end
+
+  def again
+    puts "値が不正です。もう一度入力してください。"
+  end
+
+  def even
+    puts "引き分けです。もう一度じゃんけんしましょう。"
   end
 end
 
+class Player
+  def hand
+    # puts "数字を入力してください。"
+    input_hand = gets.chomp
+    player_hand = input_hand
+  end
+end
 
 class Enemy
   def hand
@@ -31,31 +31,48 @@ end
 
 class Janken
   def pon(player_hand, enemy_hand)
-    # puts "b:#{player_hand}"
-    # puts "c:#{enemy_hand}"
-    result = (player_hand - enemy_hand +3)%3
-    # puts "d:#{result}"
-    janken = {0=>"グー",1=>"チョキ",2=>"パー"}
+    #ponを定義する時に引数を２つ受け取った状態にしているから。
+    if player_hand != "0" && player_hand != "1" && player_hand != "2"
+      interface = Interface.new
+      interface.again
+      rock_scissors_paper = Rock_scissors_paper.new
+      rock_scissors_paper.rock_scissors_paper
+    else
+      player_hand = player_hand.to_i
+      puts "#{player_hand}"
+      result = (player_hand - enemy_hand +3)%3
+      janken = {0=>"グー",1=>"チョキ",2=>"パー"}
     if result == 2
       puts "相手の手は#{janken[enemy_hand]}です。あなたの勝ちです。"
-      return false
     elsif result == 0
-      puts "引き分けです。もう一度じゃんけんしましょう。"
-      return true
+      # puts "引き分けです。もう一度じゃんけんしましょう。"
+      interface = Interface.new
+      interface.even
+      rock_scissors_paper = Rock_scissors_paper.new
+      rock_scissors_paper.rock_scissors_paper
     else
       puts "相手の手は#{janken[enemy_hand]}です。あなたの負けです。"
-      return false
+    end
     end
   end
 end
 
-player = Player.new
-enemy = Enemy.new
-janken = Janken.new
-
-next_game = true
-while next_game do
-  next_game = janken.pon(player.hand, enemy.hand)
+class Rock_scissors_paper
+  def rock_scissors_paper
+    janken = {0=>"グー",1=>"チョキ",2=>"パー"}
+    janken.each do |key, value|
+      puts "#{key}:#{value}"
+    end
+    # player ＝　Player.new
+    # enemy = Enemy.new
+    janken = Janken.new
+    janken.pon(Player.new.hand, Enemy.new.hand)
+    #ponメソッドに引数を２つ渡す必要があるので、呼び出す時は引数二つ渡す。
+  end
 end
 
-# janken.pon(player.hand, enemy.hand)←ここでもう一度記入すると勝負がついても仕切り直しになる。
+#ファイルを呼び出す時下記が呼び出される。(表示したい順番厳守)
+interface = Interface.new
+interface.first
+rock_scissors_paper = Rock_scissors_paper.new
+rock_scissors_paper.rock_scissors_paper
